5cd5d0742761abcbfdac05ca1ca8091ed9c19c9a
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-02-15 11:39:18 -0300

    initial commit

---
+++ src/main/kotlin/com/kalculator/kalculator/KAlculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.kalculator.kalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KAlculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<KAlculatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/kalculator/kalculator/KAlculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kalculator.kalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KAlculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
